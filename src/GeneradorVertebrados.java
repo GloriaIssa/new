62a33ff4cfe125ccaafefd9883d0b1fce3f2893a
GloriaIssa <87575468+GloriaIssa@users.noreply.github.com>
2021-09-26 19:02:04 -0500

    cambie vaca  por elefante

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -2,7 +2,7 @@ public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados() {
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
-        miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
+        miVertebrados[1] = new Vertebrado("Elefante", "Herviboro");
         miVertebrados[2] = new Vertebrado("perro", "gato");
 
         return miVertebrados;
